35e8ec8db6dc99a6d430f82f12deaafac379e959
1234beth <elizachela98@gmail.com>
2023-03-17 13:29:15 -0700

    6-print

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int d;
+
+for(d = 0; d <= 9; d++)
+putchar(d);
+putcha("\n");
+return (0);
+}
