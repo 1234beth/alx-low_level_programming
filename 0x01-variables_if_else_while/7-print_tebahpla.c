fbf40912ac10b2bcbcd52f5ee9b3a273dcc8d85d
1234beth <elizachela98@gmail.com>
2023-03-19 08:26:04 -0700

    7-print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+char w;
+for(w = "z"; w >= "a"; w--)
+putchar(w);
+putchar("\n");
+return (0);
+}
