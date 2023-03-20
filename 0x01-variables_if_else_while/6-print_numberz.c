5fc504bdd79f7aaf9ca95cfd3cd75aacbca8d39c
1234beth <elizachela98@gmail.com>
2023-03-20 12:05:14 -0700

    6-print

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,8 +12,8 @@ int main(void)
 
 int c;
 
-for(c = 0; c <= 9; c++)
+for(c = '0'; c <= '9'; c++)
 putchar(c);
-putchar("\n");
+putchar('\n');
 return (0);
 }
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+for (n = "0"; n <= "9"; n++)
+{
+putchar(n);
+if(n != "9")
+{
+putchar( ",");
+putchar(" ");
+}
+}
+putchar("\n");
+return (0);
+}
