b2f349e819a05f3463dc19bffa4cfab0e5d1d30f
1234beth <elizachela98@gmail.com>
2023-03-20 12:11:33 -0700

    7-print

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,9 +5,9 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-char w;
-for(w = "z"; w >= "a"; w--)
-putchar(w);
+char low;
+for (low = 'z'; low >= 'a'; low--)
+putchar(low);
 putchar("\n");
 return (0);
 }
