e6a427f7e9ba61967ef281002667c36bca3cd463
1234beth <elizachela98@gmail.com>
2023-05-01 06:03:46 -0700

    print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,61 +1,61 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely.
- *
- * @head: A pointer to the head of the listint_t list.
- *
- * Return: The number of nodes in the listint_t list.
- */
+* print_listint_safe - Prints a listint_t linked list safely.
+*
+* @head: A pointer to the head of the listint_t list.
+*
+* Return: The number of nodes in the listint_t list.
+*/
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t node_count = 0;
-    const listint_t *slow_ptr, *fast_ptr;
+size_t node_count = 0;
+const listint_t *slow_ptr, *fast_ptr;
 
-    if (head == NULL)
-        exit(98);
+if (head == NULL)
+exit(98);
 
-    slow_ptr = head;
-    fast_ptr = head->next;
+slow_ptr = head;
+fast_ptr = head->next;
 
-    while (fast_ptr != NULL && fast_ptr < slow_ptr)
-    {
-        printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
-        node_count++;
-        slow_ptr = slow_ptr->next;
-        fast_ptr = fast_ptr->next;
-        if (fast_ptr != NULL)
-            fast_ptr = fast_ptr->next;
-    }
+while (fast_ptr != NULL && fast_ptr != slow_ptr)
+{
+printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
+node_count++;
+slow_ptr = slow_ptr->next;
+fast_ptr = fast_ptr->next;
+if (fast_ptr != NULL)
+fast_ptr = fast_ptr->next;
+}
 
-    if (fast_ptr != NULL)
-    {
-        printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
-        node_count++;
-        slow_ptr = slow_ptr->next;
-        fast_ptr = fast_ptr->next;
+if (fast_ptr != NULL)
+{
+printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
+node_count++;
+slow_ptr = slow_ptr->next;
+fast_ptr = fast_ptr->next;
 
-        while (slow_ptr != fast_ptr)
-        {
-            printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
-            node_count++;
-            slow_ptr = slow_ptr->next;
-            fast_ptr = fast_ptr->next;
-        }
+while (slow_ptr != fast_ptr)
+{
+printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
+node_count++;
+slow_ptr = slow_ptr->next;
+fast_ptr = fast_ptr->next;
+}
 
-        printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
-        node_count++;
-    }
-    else
-    {
-        while (slow_ptr != NULL)
-        {
-            printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
-            node_count++;
-            slow_ptr = slow_ptr->next;
-        }
-    }
+printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
+node_count++;
+}
 
-    return (node_count);
+while (slow_ptr != NULL)
+{
+printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
+node_count++;
+slow_ptr = slow_ptr->next;
+}
+
+return (node_count);
 }
 
